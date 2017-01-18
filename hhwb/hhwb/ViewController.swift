b89d2038f75ba3aa55b1a644496218141c207a6f
forlanhan <huhan@huhandeMacBook-Pro.local>
2017-01-18 10:25:08 +0800

    初始化

---
+++ hhwb/hhwb/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  hhwb
+//
+//  Created by 胡瀚 on 2017/1/18.
+//  Copyright © 2017年 胡瀚. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
